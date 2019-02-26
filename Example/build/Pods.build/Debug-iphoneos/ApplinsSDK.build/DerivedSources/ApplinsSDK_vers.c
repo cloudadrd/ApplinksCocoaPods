106a6a3011201eaafedec363856486a4f0949a6d
lanxuping <lxp120310@163.com>
2019-02-26 14:09:11 +0800

    4.0.6

---
+++ Example/build/Pods.build/Debug-iphoneos/ApplinsSDK.build/DerivedSources/ApplinsSDK_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char ApplinsSDKVersionString[];
+ extern const double ApplinsSDKVersionNumber;
+
+ const unsigned char ApplinsSDKVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ApplinsSDK  PROJECT:Pods-1" "\n";
+ const double ApplinsSDKVersionNumber __attribute__ ((used)) = (double)1.;
